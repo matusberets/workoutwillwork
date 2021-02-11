4db7a5c05e43d8bf9be9d020cb50ec7ecf413546
matusberets <matusberets@gmail.com>
2021-02-11 19:17:49 +0100

    Session changet

--- helpers.py
+++ helpers.py
@@ -40,6 +40,5 @@ def get_db():
     """Opens a new database connection if there is none yet for the current application context."""
     if not hasattr(g, 'psql'):
         g.psql = connect_db()
-        print("Connection restabilished !")
-    print("Connection estabilished !")
+    print("Connection restabilished !")
     return g.psql
