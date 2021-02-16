42a790dea7da147daac2517592bc1d8abdb18284
matusberets <matusberets@gmail.com>
2021-02-16 18:23:50 +0100

    Session file dir omitting

--- application.py
+++ application.py
@@ -13,6 +13,7 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from helpers import login_required, error, connect_db, get_db, debug_print, g
 
 
+
 # Configure application
 app = Flask(__name__)
 
@@ -28,7 +29,7 @@ def after_request(response):
     return response
 
 # Configure session to use filesystem (instead of signed cookies)
-app.config["SESSION_FILE_DIR"] = mkdtemp()
+# app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
